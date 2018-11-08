c70d3e1bb0cc6f04791f95c870477e7aba7b3272
CrishFK <cristiansanchez93@hotmail.es>
2018-11-08 10:25:57 +0100

    Init Proyect

---
+++ Exercise1/Exercise1/Menu_Principal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise1
+{
+    public partial class Menu_Principal : Form
+    {
+        public Menu_Principal()
+        {
+            InitializeComponent();
+        }
+    }
+}
