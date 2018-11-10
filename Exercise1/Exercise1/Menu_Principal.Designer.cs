98c92dad8ff2438003600e4eac634c11a6161973
Cristian Sanchez <cristiansanchez93@hotmail.es>
2018-11-10 18:04:16 +0100

    Creacion de interfaz y Clase coche

--- Exercise1/Exercise1/Menu_Principal.Designer.cs
+++ Exercise1/Exercise1/Menu_Principal.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Menu_Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Menu_Principal";
+            this.Text = "Menu Principal";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
