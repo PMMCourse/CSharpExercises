aaa356c986aba8ae4f89702e8d54d9e69e956cb7
mastermartin11 <43747611+mastermartin11@users.noreply.github.com>
2018-11-13 17:48:03 +0100

    v.1.0 terminado(falta revision y limpieza)

--- main/main/Form1.Designer.cs
+++ main/main/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.components = new System.ComponentModel.Container();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.carBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.button_add = new System.Windows.Forms.Button();
             this.button_remove = new System.Windows.Forms.Button();
@@ -41,12 +40,12 @@
             this.comboBox5 = new System.Windows.Forms.ComboBox();
             this.comboBox6 = new System.Windows.Forms.ComboBox();
             this.button_done = new System.Windows.Forms.Button();
+            this.carBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.carBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // listBox1
             // 
-            this.listBox1.DataSource = this.carBindingSource;
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
             this.listBox1.Location = new System.Drawing.Point(37, 153);
@@ -54,10 +53,6 @@
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(231, 340);
             this.listBox1.TabIndex = 0;
-            // 
-            // carBindingSource
-            // 
-            this.carBindingSource.DataSource = typeof(main.Car);
             // 
             // listBox2
             // 
@@ -99,6 +94,7 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 24);
             this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // comboBox2
             // 
@@ -108,6 +104,7 @@
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 24);
             this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // comboBox3
             // 
@@ -117,6 +114,7 @@
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(121, 24);
             this.comboBox3.TabIndex = 6;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // comboBox4
             // 
@@ -126,28 +124,27 @@
             this.comboBox4.Name = "comboBox4";
             this.comboBox4.Size = new System.Drawing.Size(121, 24);
             this.comboBox4.TabIndex = 7;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
             // 
             // comboBox5
             // 
-            this.comboBox5.DataSource = this.carBindingSource;
-            this.comboBox5.DisplayMember = "Model";
             this.comboBox5.FormattingEnabled = true;
             this.comboBox5.Location = new System.Drawing.Point(491, 78);
             this.comboBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox5.Name = "comboBox5";
             this.comboBox5.Size = new System.Drawing.Size(121, 24);
             this.comboBox5.TabIndex = 8;
+            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
             // 
             // comboBox6
             // 
-            this.comboBox6.DataSource = this.carBindingSource;
-            this.comboBox6.DisplayMember = "Color";
             this.comboBox6.FormattingEnabled = true;
             this.comboBox6.Location = new System.Drawing.Point(491, 122);
             this.comboBox6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox6.Name = "comboBox6";
             this.comboBox6.Size = new System.Drawing.Size(121, 24);
             this.comboBox6.TabIndex = 9;
+            this.comboBox6.SelectionChangeCommitted += new System.EventHandler(this.comboBox6_SelectedIndexChanged);
             // 
             // button_done
             // 
@@ -158,6 +155,10 @@
             this.button_done.Text = "done";
             this.button_done.UseVisualStyleBackColor = true;
             this.button_done.Click += new System.EventHandler(this.button_done_Click);
+            // 
+            // carBindingSource
+            // 
+            this.carBindingSource.DataSource = typeof(main.Car);
             // 
             // Form1
             // 
--- main/main/Form1.cs
+++ main/main/Form1.cs
@@ -17,46 +17,62 @@ namespace main
 {
     public partial class Form1 : Form
     {
+        int contador1=0;
+        int contador2=0;
+        int contador3=0;
+        List<Car> list_coches;
+        List<Car> list_coches2=new List<Car>();
+        
+        List<Car> Cars2= new List<Car>();
+        List<Car> Cars;
+
         public Form1()
         {
             InitializeComponent();
             
             string outputJSON = File.ReadAllText(@"..\..\..\..\Content\Cars.json");
-            List<Car> Cars = JsonConvert.DeserializeObject<List<Car>>(outputJSON);
-
-            foreach (Car cars_values in Cars) {
-
-                if (!comboBox1.Items.Contains(cars_values.Maker))
-                {
-                    comboBox1.Items.Add(cars_values.Maker);
-
-                }
-                if (!comboBox2.Items.Contains(cars_values.Model))
-                {
-                    comboBox2.Items.Add(cars_values.Model);
-
-                }
-                if ((cars_values.Color != null)&&!comboBox3.Items.Contains(cars_values.Color))
-                {
-
-                    comboBox3.Items.Add(cars_values.Color);
-
-                }
+             Cars = JsonConvert.DeserializeObject<List<Car>>(outputJSON);
 
 
+            var coches_maker = Cars.Select(x => x.Maker).Distinct().ToList();
+            var coches_model = Cars.Select(x => x.Model).Distinct().ToList();
+            var coches_color = Cars.Select(x => x.Color).Distinct().Where(y => y != null).ToList();
+
+            
+
+            comboBox1.Items.Add("not selected");
+            comboBox2.Items.Add("not selected");
+            comboBox3.Items.Add("not selected");
+            comboBox4.Items.Add("not selected");
+            comboBox5.Items.Add("not selected");
+            comboBox6.Items.Add("not selected");
+            /*
+            
+            comboBox4.SelectedIndex = 0;
+            comboBox5.SelectedIndex = 0;
+            comboBox6.SelectedIndex = 0;
+            */
+            
+
+            foreach (String coche in coches_maker) {
+                comboBox1.Items.Add(coche);        
             }
+            comboBox1.SelectedIndex = 0;
 
             
-           
+            foreach (String coche in coches_model)
+            {
+                comboBox2.Items.Add(coche);
+            }
+            comboBox2.SelectedIndex = 0;
             
-            listBox1.DataSource = Cars;
+          
+            foreach (String coche in coches_color)
+            {
+                comboBox3.Items.Add(coche);
+            }
+            comboBox3.SelectedIndex = 0;
 
-
-            
-
-
-
-            
             
         }
 
@@ -66,23 +82,93 @@ namespace main
             int num_cars = listBox2.Items.Count;
             Boolean not_Found = true;
             
-                for (int i = 0; i <num_cars; i++)
+            for (int i = 0; i < num_cars; i++)
+            {
+                if (listBox1.SelectedItem.Equals(listBox2.Items[i]))
                 {
-                    if (listBox1.SelectedItem.Equals(listBox2.Items[i]))
-                    {
-                     not_Found = false;
-                    }
-                    
+                    not_Found = false;
                 }
-                if (not_Found)
-                    {
-                      listBox2.Items.Add(listBox1.SelectedItem);
-                    }
+
+            }
+            if (not_Found)
+            {
+
+                Cars2.Add((Car)listBox1.SelectedItem);
+
+                listBox2.Items.Add(Cars2.Last());
+                
+
+
+                comboBox4.Items.Clear();
+                comboBox5.Items.Clear();
+                comboBox6.Items.Clear();
+
+                comboBox4.Items.Add("not selected");
+                comboBox5.Items.Add("not selected");
+                comboBox6.Items.Add("not selected");
+
+                var coches_maker2 = Cars2.Select(x => x.Maker).Distinct().ToList();
+                var coches_model2 = Cars2.Select(x => x.Model).Distinct().ToList();
+                var coches_color2 = Cars2.Select(x => x.Color).Distinct().Where(y => y != null).ToList();
+
+                foreach (String coches in coches_maker2) {
+                    comboBox4.Items.Add(coches);
+                }
+
+                foreach (String coches in coches_model2)
+                {
+                    comboBox5.Items.Add(coches);
+                }
+                foreach (String coches in coches_color2)
+                {
+                    comboBox6.Items.Add(coches);
+                }
+            }
+            
+            
+
+
+
         }
 
-        private void button_remove_Click(object sender, EventArgs e)
+
+          private void button_remove_Click(object sender, EventArgs e)
         {
+            comboBox4.Items.Clear();
+            comboBox5.Items.Clear();
+            comboBox6.Items.Clear();
+
+            Cars2.Remove((Car)listBox2.SelectedItem);
             listBox2.Items.Remove(listBox2.SelectedItem);
+            
+            
+            
+            var coches_maker2 = Cars2.Select(x => x.Maker).Distinct().ToList();
+            var coches_model2 = Cars2.Select(x => x.Model).Distinct().ToList();
+            var coches_color2 = Cars2.Select(x => x.Color).Distinct().Where(y => y != null).ToList();
+
+           
+
+            comboBox4.Items.Add("not selected");
+            comboBox5.Items.Add("not selected");
+            comboBox6.Items.Add("not selected");
+
+
+            foreach (String coches in coches_maker2)
+            {
+                comboBox4.Items.Add(coches);
+            }
+
+            foreach (String coches in coches_model2)
+            {
+                comboBox5.Items.Add(coches);
+            }
+            foreach (String coches in coches_color2)
+            {
+                comboBox6.Items.Add(coches);
+            }
+
+
         }
 
         private void button_done_Click(object sender, EventArgs e)
@@ -93,6 +179,277 @@ namespace main
             cars_list windows_car_list = new cars_list(data);
 
             windows_car_list.ShowDialog();
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            
+            var marca = comboBox1.SelectedItem;
+            var modelo = comboBox2.SelectedItem;
+            var color = comboBox3.SelectedItem;
+
+            if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex==0 && comboBox3.SelectedIndex==0)
+            {
+                list_coches = Cars;
+            }
+
+            else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex==0)
+            {
+                list_coches = Cars.Select(y => y).Where(x =>(x.Color != null && x.Color.Equals(color))).ToList();
+            }
+            else if  (comboBox1.SelectedIndex == 0 && comboBox3.SelectedIndex==0)
+            {
+                list_coches = Cars.Select(y => y).Where(x => (x.Model.Equals(modelo))).ToList();
+            }
+            
+            else if (comboBox2.SelectedIndex == 0 && comboBox3.SelectedIndex == 0)
+            {
+                list_coches = Cars.Select(y => y).Where(x => (x.Maker.Equals(marca))).ToList();
+            }
+            else if (comboBox3.SelectedIndex == 0 )
+            {
+                list_coches = Cars.Select(y => y).Where(x => (x.Maker.Equals(marca)) && (x.Model.Equals(modelo))).ToList();
+            }
+
+            else if (comboBox2.SelectedIndex == 0)
+            {
+                list_coches = Cars.Select(y => y).Where(x => (x.Maker.Equals(marca)) && (x.Color != null && x.Color.Equals(color))).ToList();
+            }
+
+            else if (comboBox1.SelectedIndex == 0)
+            {
+                list_coches = Cars.Select(y => y).Where(x => (x.Color != null && x.Color.Equals(color)) && (x.Model.Equals(modelo))).ToList();
+            }
+
+
+            // list_coches = Cars.Select(y => y).Where(x => (x.Maker.Equals(marca)) && (x.Model.Equals(modelo)) && (x.Color != null && x.Color.Equals(color))).ToList();
+
+
+
+            listBox1.DataSource = null;
+            listBox1.DataSource = list_coches;
+
+
+            
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (contador1 == 0)
+            {
+
+                contador1++;
+                contador2++;
+                contador3++;
+
+                comboBox5.SelectedIndex = 0;
+                comboBox6.SelectedIndex = 0;
+
+
+
+            }
+
+            var marca = comboBox4.SelectedItem;
+            var modelo = comboBox5.SelectedItem;
+            var color = comboBox6.SelectedItem;
+
+            list_coches2 = new List<Car>();
+
+            if (comboBox4.SelectedIndex == 0 && comboBox5.SelectedIndex == 0 && comboBox6.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2;
+            }
+
+            else if (comboBox4.SelectedIndex == 0 && comboBox5.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Color != null && x.Color.Equals(color))).ToList();
+            }
+            else if (comboBox4.SelectedIndex == 0 && comboBox6.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Model.Equals(modelo))).ToList();
+            }
+
+            else if (comboBox5.SelectedIndex == 0 && comboBox6.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Maker.Equals(marca))).ToList();
+            }
+            else if (comboBox6.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Maker.Equals(marca)) && (x.Model.Equals(modelo))).ToList();
+            }
+
+            else if (comboBox5.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Maker.Equals(marca)) && (x.Color != null && x.Color.Equals(color))).ToList();
+            }
+
+            else if (comboBox4.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Color != null && x.Color.Equals(color)) && (x.Model.Equals(modelo))).ToList();
+            }
+
+            else if (comboBox4.SelectedIndex != 0 && comboBox5.SelectedIndex != 0 && comboBox6.SelectedIndex != 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Maker.Equals(marca)) && (x.Model.Equals(modelo)) && (x.Color != null && x.Color.Equals(color))).ToList();
+            }
+
+
+            listBox2.Items.Clear();
+
+            foreach (Car coches in list_coches2)
+            {
+                listBox2.Items.Add(coches);
+            }
+
+
+        }
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            if (contador2 == 0)
+            {
+
+                contador1++;
+                contador2++;
+                contador3++;
+
+                comboBox4.SelectedIndex = 0;
+                comboBox6.SelectedIndex = 0;
+                
+                
+               
+            }
+            
+
+            var marca = comboBox4.SelectedItem;
+            var modelo = comboBox5.SelectedItem;
+            var color = comboBox6.SelectedItem;
+
+            list_coches2 = new List<Car>();
+
+            if (comboBox4.SelectedIndex == 0 && comboBox5.SelectedIndex == 0 && comboBox6.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2;
+            }
+
+            else if (comboBox4.SelectedIndex == 0 && comboBox5.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Color != null && x.Color.Equals(color))).ToList();
+            }
+            else if (comboBox4.SelectedIndex == 0 && comboBox6.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Model.Equals(modelo))).ToList();
+            }
+
+            else if (comboBox5.SelectedIndex == 0 && comboBox6.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Maker.Equals(marca))).ToList();
+            }
+            else if (comboBox6.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Maker.Equals(marca)) && (x.Model.Equals(modelo))).ToList();
+            }
+
+            else if (comboBox5.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Maker.Equals(marca)) && (x.Color != null && x.Color.Equals(color))).ToList();
+            }
+
+            else if (comboBox4.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Color != null && x.Color.Equals(color)) && (x.Model.Equals(modelo))).ToList();
+            }
+
+            else if (comboBox4.SelectedIndex != 0 && comboBox5.SelectedIndex != 0 && comboBox6.SelectedIndex != 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Maker.Equals(marca)) && (x.Model.Equals(modelo)) && (x.Color != null && x.Color.Equals(color))).ToList();
+            }
+
+
+            listBox2.Items.Clear();
+
+            foreach (Car coches in list_coches2)
+            {
+                listBox2.Items.Add(coches);
+            }
+            
+
+
+        }
+
+        private void comboBox6_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            if (contador3 == 0)
+            {
+
+                contador1++;
+                contador2++;
+                contador3++;
+
+                comboBox5.SelectedIndex = 0;
+                comboBox4.SelectedIndex = 0;
+                
+                
+                
+            }
+
+            var marca = comboBox4.SelectedItem;
+            var modelo = comboBox5.SelectedItem;
+            var color = comboBox6.SelectedItem;
+
+            list_coches2 = new List<Car>();
+
+            if (comboBox4.SelectedIndex == 0 && comboBox5.SelectedIndex == 0 && comboBox6.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2;
+            }
+
+            else if (comboBox4.SelectedIndex == 0 && comboBox5.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Color != null && x.Color.Equals(color))).ToList();
+            }
+            else if (comboBox4.SelectedIndex == 0 && comboBox6.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Model.Equals(modelo))).ToList();
+            }
+
+            else if (comboBox5.SelectedIndex == 0 && comboBox6.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Maker.Equals(marca))).ToList();
+            }
+            else if (comboBox6.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Maker.Equals(marca)) && (x.Model.Equals(modelo))).ToList();
+            }
+
+            else if (comboBox5.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Maker.Equals(marca)) && (x.Color != null && x.Color.Equals(color))).ToList();
+            }
+
+            else if (comboBox4.SelectedIndex == 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Color != null && x.Color.Equals(color)) && (x.Model.Equals(modelo))).ToList();
+            }
+
+            else if (comboBox4.SelectedIndex != 0 && comboBox5.SelectedIndex != 0 && comboBox6.SelectedIndex != 0)
+            {
+                list_coches2 = Cars2.Select(y => y).Where(x => (x.Maker.Equals(marca)) && (x.Model.Equals(modelo)) && (x.Color != null && x.Color.Equals(color))).ToList();
+            }
+
+            
+            listBox2.Items.Clear();
+
+            foreach (Car coches in list_coches2)
+            {
+                listBox2.Items.Add(coches);
+            }
+            
 
 
         }
